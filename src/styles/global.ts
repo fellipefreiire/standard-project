31ec88cda1bf04affa6f4c2363f142a3a244c051
Felipe Freire <fellipefreiire3@gmail.com>
2021-06-01 14:55:32 -0300

    update

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,9 +7,34 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  @media(max-width: 1080px) {
+    html {
+      font-size: 93.75% // 15px
+    }
+  }
+
+  @media(max-width: 720px) {
+    html {
+      font-size: 87.5% // 15px
+    }
+  }
+
   body {
     background: ${props => props.theme.colors.background};
+  }
+
+  body, input, textarea, button {
+    font: 500 1rem Roboto, sans-serif;
     color: ${props => props.theme.colors.text};
-    font: 400 16px Roboto, sans-serif;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+  h2 {
+    font-size: 1.5rem;
+  }
+  button {
+    cursor: pointer;
   }
 `
